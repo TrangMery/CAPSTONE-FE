4eddc9e2103baffa868c5125e4a90f424c98de2c
nguyen trong duy <76764193+hotdog789ag@users.noreply.github.com>
2024-07-31 23:34:16 +0700

    fix bug experid token

--- src/utils/axios-customize.js
+++ src/utils/axios-customize.js
@@ -1,3 +1,5 @@
+
+import { notification } from "antd";
 import axios from "axios";
 
 const baseURL = import.meta.env.VITE_BACKEND_URL;
@@ -29,7 +31,7 @@ instance.interceptors.request.use(
     return Promise.reject(error);
   }
 );
-
+const NO_RETRY_HEADER = "x-no-retry";
 // Add a response interceptor
 instance.interceptors.response.use(
   function (response) {
@@ -40,6 +42,21 @@ instance.interceptors.response.use(
   function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
+    if (
+      error.config &&
+      error.response &&
+      +error.response.status === 401 &&
+      !error.config.headers[NO_RETRY_HEADER]
+    ) {
+      error.config.headers[NO_RETRY_HEADER] = "true";
+      notification.info({
+        description: "Vui lòng đăng nhập lại",
+        message: "Hết phiên đăng nhập",
+      })
+      localStorage.removeItem("token");
+      sessionStorage.removeItem("userId");
+      window.location.href = "/login";
+    }
     return error?.response?.data ?? Promise.reject(error);
   }
 );

a6bd314ddb13590c8edd7d08d67ca9dca6d112a4
nguyen trong duy <76764193+hotdog789ag@users.noreply.github.com>
2024-07-31 22:51:31 +0700

    fix bug file

--- src/components/admin/modalUploadUser.jsx
+++ src/components/admin/modalUploadUser.jsx
@@ -219,7 +219,7 @@ const UploadByFile = (props) => {
     },
   };
   const url =
-    "https://srms2.sgp1.cdn.digitaloceanspaces.com/Book1-20240721161353428.xlsx";
+    "https://srms2.sgp1.cdn.digitaloceanspaces.com/Book1-20240731224955780.xlsx";
   return (
     <>
       <Modal
